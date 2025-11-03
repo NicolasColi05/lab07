7aa781baf015d366f9ff826abccf1810a14e166d
Nicolas Colinucci <nicolascolinucci@MacBook-Air-di-Nicolas.local>
2025-11-03 13:54:52 +0100

    risolto problema del ultimo punto

--- 71-custom-iterable/src/main/java/it/unibo/inner/test/impl/IterableWithPolicyImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/test/impl/IterableWithPolicyImpl.java
@@ -1,21 +1,31 @@
 package   it.unibo.inner.test.impl;
 
-
 import java.util.Iterator;
 
-
+import it.unibo.inner.api.Predicate;
 import it.unibo.inner.api.IterableWithPolicy;
 
 public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T>{
 
     private T[] array;
+    private Predicate<T> filter;
     public IterableWithPolicyImpl(T[] array){
+        
+        this(array , new Predicate<T>() {
+            public boolean test(T t){
+                return true;
+            }
+        });
+    }
+    public IterableWithPolicyImpl(T[] array , Predicate<T> p){
         this.array = array;
+        this.filter = p;
+        //boolean b = filter.test(array[0]);
     }
     @Override
     public void setIterationPolicy(it.unibo.inner.api.Predicate<T> filter) {
         
-        throw new UnsupportedOperationException("Unimplemented method 'setIterationPolicy'");
+        this.filter = filter;
     }
     
     public Iterator<T> iterator(){
@@ -26,18 +36,27 @@ public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T>{
         private int index ;
 
         public boolean hasNext(){
-            return this.index < array.length;
-        }
+            // TODO pensare a un modo per far partire e terminsre l'iteratore solo se è true il filtro...
+
+            while(this.index < array.length){
+               if(filter.test(array[index]) ){
+                 return this.index +1 <= array.length;
+               }else{
+                 index++;
+                 
+                }
+            
+            }
+            return false;
+        }   
         public T next(){
-            if(hasNext()){
+            
+            return array[index++];
                 
-                return array[index++];
                 
             }
-            else{
-                throw new UnsupportedOperationException("out of array");
-            }
-        }
+            
+        
     }
    
     
